a5cf7c0956c264787f136d4a62986f0860930c9c
minhyeok8521 <124540446+minhyeok8521@users.noreply.github.com>
2023-07-26 11:11:08 +0900

    3 files modified

--- src/components/AllMenu/AllMenu.js
+++ src/components/AllMenu/AllMenu.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function AllMenu() {
+  const [inOpen, setIsOpen] = useState();
+
   useEffect(() => {
     // alert('c');
-  });
-
+    console.log('cc');
+  }, [inOpen]);
+  s;
   return (
     <div className="AllMenuWrap">
       <button>메뉴</button>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="headerWrap">
       <strong>헤더</strong>
-      <AllMenu className="menu"/>
+      <AllMenu />
     </div>
   );
 }
